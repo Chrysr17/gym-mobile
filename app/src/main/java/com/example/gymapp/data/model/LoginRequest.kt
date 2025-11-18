93b69067bee3bc4d486a2550fe2c592764dfb462
Chrysr17 <chrisrasaro@gmail.com>
2025-11-17 19:52:26 -0500

    LoginRequest

---
+++ app/src/main/java/com/example/gymapp/data/model/LoginRequest.kt
@@ -0,0 +1,6 @@
+package com.example.gymapp.data.model
+
+data class LoginRequest(
+    val nombreUsuario: String,
+    val password: String
+)
--- app/src/main/java/com/example/gymapp/data/service/AuthService.kt
+++ app/src/main/java/com/example/gymapp/data/service/AuthService.kt
@@ -2,13 +2,12 @@ package com.example.gymapp.data.service
 
 import com.example.gymapp.data.model.ClienteDTO
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface AuthService {
     @POST("auth/login")
     suspend fun login(
-        @Query("nombreUsuario") nombreUsuario: String,
-        @Query("password") password: String
     ): Response<ClienteDTO>
 }
